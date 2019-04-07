c772d9cd2e513e6509220a501f0c9bcf2727c10c
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-07 19:56:55 +0000

    Snapshot of branch master taken on 2019-04-07T15:56:53.913-04:00 from "https://github.com/UnofficialJuliaMirror/CompoundPeriods.jl-a216cea6-0a8c-5945-ab87-5ade47210022"

---
+++ docs/make.jl
@@ -0,0 +1,20 @@
+using Documenter, CompoundPeriods, Dates
+
+makedocs(
+    modules = [CompoundPeriods],
+    sitename = "CompoundPeriods",
+    pages  = Any[
+        "Overview"                           => "index.md",
+        "Canonical Compound Periods"         => "canonicalcompoundperiods.md",
+        "Reverse Compound Periods"           => "reversecompoundperiods.md",
+        "Well Behaved Temporal Composites"   => "wellbehavedtemporalcomposites.md",
+        "Nanosecond Increments"              => "nanosecondincrements.md",
+        "min, max, minmax"                   => "minmaxminmax.md",
+        "Dates, Times"                       => "datestimes.md"
+        ]
+    )
+
+deploydocs(
+    repo = "github.com/JeffreySarnoff/CompoundPeriods.jl.git",
+    target = "build"
+)
